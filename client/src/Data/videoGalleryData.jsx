17ef07428448a1234b583f27217c929440b61bbb
Nabin Maharjan <maharjan.nabin85@gmail.com>
2023-12-10 16:45:14 +0545

    image and video gallery section added

---
+++ client/src/Data/videoGalleryData.jsx
@@ -0,0 +1,9 @@
+const VideoData = [
+    {
+        'id': 1,
+       'videoDetail': '/videoGallery/video1.mp4'
+    },
+   
+   
+]
+export default VideoData
--- client/src/Pages/Gallery/VideoGallery.jsx
+++ client/src/Pages/Gallery/VideoGallery.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
-const VideoGallery = () => {
+import videoGalleryData from "../../Data/videoGalleryData"
+const Introduction = () => {
   return (
-    <div>VideoGallery</div>
-  )
-}
+    <>
+      <div className="bg-blue-700">
+        <div className="container mx-auto">
+          <div className="py-8 flex justify-center">
+            <div>
+              <h1 className="text-5xl text-white tracking-wider">
+                VIDEO GALLERY (भिडियो ग्यालरी)
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mx-auto py-16 flex flex-wrap gap-4 ">
+        {videoGalleryData.map((item) => (
+          <div className="" key={item.id}>
+            <div className="relative w-[360px] sm:w-[23rem] h-72 overflow-hidden">
+              <video src={item.videoDetail} controls></video>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default VideoGallery
+export default Introduction;
